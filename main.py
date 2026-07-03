71e7a69fb45a1912d775ea2a4dc88d5066c912a4
Arian <ariyanesmaeelitrade1380@gmail.com>
2026-07-03 08:58:17 -0700

    calcualting average and total marks in python

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-x = int(input ("age:"))
+marks = [15,20,12,18,15]
 
-if x >= 18:
-    print("welcome")
-else:
-    print("you are not eligible")
+total_sum = 0
+
+for mark in marks:
+    total_sum += mark
+
+    average = total_sum / len(marks)
+
+print(total_sum)
+print(average)
