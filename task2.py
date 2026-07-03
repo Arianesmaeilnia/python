26c528a659af1c562108085cbc718c5d3ded99a9
Arian <ariyanesmaeelitrade1380@gmail.com>
2026-07-03 09:19:48 -0700

    task 2 practising python

---
+++ task2.py
@@ -0,0 +1,12 @@
+numbers = [1,7,4,12,9,18,5,20]
+
+even_numbers = []
+
+for number in numbers:
+
+    if number % 2 == 0:
+
+     even_numbers.append(number)
+
+print("even_numbers list:")
+print(even_numbers)
